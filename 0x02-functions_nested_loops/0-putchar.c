c5a3f8c8c8254687a9df630f147624f4b379e97f
amiinh8 <amiinh8@gmail.com>
2022-03-16 13:16:09 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,15 @@
 #include "main.h"
 
+
 int main(void)
-  (
-        char chaine[] = "_putchar";
+{
+	char chaine[] = "_putchar\n";
 	int i = 0;
 
 	while (chaine[i])
-	  {
-
-	    _putchar(chaine[i]);
-	    i++
-
-
-	  }
-	  return(0);   
-)
+	{
+		_putchar(chaine[i]);
+		i++;
+	}
+	return (0);
+}
