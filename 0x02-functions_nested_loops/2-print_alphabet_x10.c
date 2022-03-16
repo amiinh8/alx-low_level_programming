86ed11049b9265aa0b8b999d76eb4bf500ac4570
amiinh8 <amiinh8@gmail.com>
2022-03-16 16:45:43 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+#include<stdio.h>
+
+/**
+ *print_alphabet_x10 - function that prints 10 times the alphabet,
+ *in lowercase, followed by a new line.
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+        
+	for (i = 0; i <= 9; i++)
+	  return (0);
+}
