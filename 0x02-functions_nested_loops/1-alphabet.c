dc57da55add18917142c829342298a7385dd22dd
amiinh8 <amiinh8@gmail.com>
2022-03-16 16:35:32 -0700

    prints the alphabet, in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,11 @@
 #include "main.h"
 
-/**
- * print_alphabet -  prints the alphabet, in lowercase, followed by a new line
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
 
 void print_alphabet(void)
 {
 	int i;
 
-	for (i = 97; i <= 122; i++)
+	for (i = 1; i <= 25; i++)
 		_putchar(i);
 	_putchar('\n');
 }
