f23f726194697f7a075b5c5f561be62e2ae1240c
amiinh8 <amiinh8@gmail.com>
2022-04-07 13:28:04 -0700

    function that concatenates two strings function that concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,19 +1,69 @@
 #include "main.h"
 /**
- *malloc_checked - array for prints a string
- *@b: number of memory
- *Return: void
+ *_strlen - count array
+ *@s: array of elements
+ *Return: int i
  */
-
-void *malloc_checked(unsigned int b)
+int _strlen(char *s)
 {
-void *p;
+unsigned int i;
 
-p = malloc(b);
-if (p == NULL)
+i = 0;
+while (s[i] != '\0') /*Count charater of string*/
 {
-exit(98);
+i++;
+}
+return (i);
 }
 
-return (p);
+/**
+ *string_nconcat - concatenates two strings
+ *@s1: string
+ *@s2: string
+ *@n: number bytes
+ *Return: char dst
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+char *dst;
+unsigned int i, count, j, size, size_1, size_2;
+
+if (s1 == NULL) /*If the array is empty*/
+s1 = "";
+
+if (s2 == NULL)
+s2 = "";
+
+size_1 = _strlen(s1);
+size_2 = _strlen(s2);
+
+if (n >= size_2)
+{
+count = size_2;
+}
+else
+{
+count = n;
+}
+/*count size total*/
+size = size_1 + count + 1;
+/*malloc*/
+dst = (char *) malloc(size * sizeof(char));
+if (dst == 0)
+{
+return (NULL);
+}
+/*Concantenate arrays*/
+{
+for (i = 0; *(s1 + i) != '\0'; i++)
+*(dst + i) = *(s1 + i);
+for (j = 0; j < count; j++)
+{
+*(dst + i) = *(s2 + j);
+i++;
+}
+*(dst + i) = '\0';
+}
+return (dst);
+
 }
